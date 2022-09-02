44a077c1805224356dc973dabcdb7732e8cfe588
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-09-02 17:32:34 +0900

    feat: cache busting 설정 추가

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.etag;
 
+import com.example.version.ResourceVersion;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +9,17 @@ import org.springframework.web.filter.ShallowEtagHeaderFilter;
 @Configuration
 public class EtagFilterConfiguration {
 
+    private final ResourceVersion resourceVersion;
+
+    public EtagFilterConfiguration(ResourceVersion resourceVersion) {
+        this.resourceVersion = resourceVersion;
+    }
+
     @Bean
     public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
         FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean
-                = new FilterRegistrationBean<>( new ShallowEtagHeaderFilter());
-        filterRegistrationBean.addUrlPatterns("/etag");
+                = new FilterRegistrationBean<>(new ShallowEtagHeaderFilter());
+        filterRegistrationBean.addUrlPatterns("/etag", "/resources/" + resourceVersion.getVersion() + "/js/index.js");
         return filterRegistrationBean;
     }
 }
