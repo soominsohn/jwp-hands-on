47ea094af2d6288b8783fd66fd6722fa06608886
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-09-02 15:47:45 +0900

    feat: Cache-Control 헤더 no-cache, private 추가 인터셉터 구현

--- cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
+++ cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
@@ -1,5 +1,6 @@
 package com.example.cachecontrol;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,5 +10,12 @@ public class CacheWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(addHeaderInterceptor())
+                .addPathPatterns("/");
+    }
+
+    @Bean
+    public AddHeaderInterceptor addHeaderInterceptor() {
+        return new AddHeaderInterceptor();
     }
 }
